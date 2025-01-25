6f37caba3c03c6681aed7dd5dde678a1595ddca1
steven <steven@tomo.inc>
2025-01-25 17:21:32 +0800

    try to connect to emulator

--- src/common/utils/transport.ts
+++ src/common/utils/transport.ts
@@ -1,41 +1,52 @@
+import SpeculosTransport from '@ledgerhq/hw-transport-node-speculos-http';
 import { type Transport } from '@secretkeylabs/xverse-core';
 
+import type { SpeculosHttpTransportOpts } from '@ledgerhq/hw-transport-node-speculos-http';
+
 interface TransportOptions {
-    // /** 是否使用 Speculos 模拟器；默认为 false */
-    // useSpeculos?: boolean;
-    // /** Speculos 的 HTTP 地址，如 http://127.0.0.1:5000 */
-    // baseURL?: string;
-    // /** HTTP Agent，可选，用于保持长连接或自定义代理 */
-    // agent?: Agent;
-    // /** 连接超时时间，单位毫秒 */
-    // timeout?: number;
-    // /** 是否允许发送任意 APDU（某些测试场景下需要） */
-    // arbitraryAPDU?: boolean;
+  /** 是否使用 Speculos 模拟器；默认为 false */
+  useSpeculos?: boolean;
+  apiPort?: string;
+}
+
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function openSpeculosAndWait(
+  opts: SpeculosHttpTransportOpts = {},
+): Promise<SpeculosTransport> {
+  for (let i = 0; ; i++) {
+    try {
+      return await SpeculosTransport.open(opts);
+    } catch (e) {
+      if (i > 50) {
+        throw e;
+      }
+    }
+    await sleep(100);
   }
+}
 
 export async function getLedgerTransport({
-//   useSpeculos = false,
-//   baseURL = 'http://127.0.0.1:5000',
-//   agent,
-//   timeout = 30000,
-//   arbitraryAPDU = false,
+  useSpeculos = true,
+  apiPort = '3344',
 }: TransportOptions = {}): Promise<Transport> {
-  console.log('---------------------------------getLedgerTransport---------------------------------')
-  // if (useSpeculos) {
-  //   // Node.js 环境下的 Speculos Transport
-  //   // 注意 require 或动态 import 防止在浏览器打包时报错
-  //   const TransportNodeSpeculos = await import("@ledgerhq/hw-transport-node-speculos-http").then(
-  //     (m) => m.default,
-  //   );
+  console.log(
+    '---------------------------------getLedgerTransport---------------------------------',
+  );
 
-  //   return await TransportNodeSpeculos.open({
-  //     baseURL,
-  //     agent,
-  //     timeout,
-  //     arbitraryAPDU,
-  //   });
-  // } else {
-  const TransportWebUSB = await import('@ledgerhq/hw-transport-webusb').then((m) => m.default);
-  return await TransportWebUSB.create() as Transport;
-  // }
+  let transport: Transport;
+  if (useSpeculos) {
+    transport = (await openSpeculosAndWait({
+      apiPort,
+    })) as Transport;
+  } else {
+    const TransportWebUSB = await import('@ledgerhq/hw-transport-webusb').then((m) => m.default);
+    transport = (await TransportWebUSB.create()) as Transport;
+  }
+
+  return transport;
 }
